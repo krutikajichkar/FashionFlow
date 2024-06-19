b812d17f451708b62427239d6f179aaf2d825bbf
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2024-06-19 20:46:01 +0530

    resolved msg bug

--- src/components/DiscountMessage.tsx
+++ src/components/DiscountMessage.tsx
@@ -12,7 +12,7 @@ const DiscountMessage = ({show , setShow} : messageProps) => {
   return (
     <div className="bg-[#1b2834] text-white py-4 sm:px-24 px-6  flex justify-between items-center">
       <span className=" w-[80%]">
-        Sign up now and enjoy a 25% discount on your first order. Don't miss
+        Sign up now and enjoy a 25% discount on your first order. Don&apos;t miss
         out! <span className="font-semibold text-blue-600">Sign up now</span>
       </span>
       <span className="text-xl cursor-pointer" onClick={handleClose}>
